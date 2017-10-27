7594a791c20b58c04f12e2a6f135702b866808e1
Kaleo Cheng <kaleocheng@gmail.com>
2017-10-27 11:22:19 +0800

    Add NewRegistry for api

--- api/registry.go
+++ api/registry.go
@@ -1,6 +1,10 @@
 package api
 
-import "github.com/kaleocheng/docker-registry-client/registry"
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/kaleocheng/docker-registry-client/registry"
+	"github.com/segmentio/ksuid"
+)
 
 // Registry ...
 type Registry struct {
@@ -16,6 +20,27 @@ func init() {
 	registryMap = make(map[string]*registry.Registry)
 }
 
+// NewRegistry ...
+func NewRegistry(r *Registry) (string, error) {
+	ok, err := RegistryExist(r)
+	if err != nil {
+		return "", err
+	}
+	if ok {
+		return r.ID, nil
+	}
+
+	hub, err := registry.New(r.URL, r.Username, r.Password)
+	if err != nil {
+		log.Error(err)
+		return "", errCreateRegistryFailed
+	}
+	id := ksuid.New()
+	r.ID = id.String()
+	AddRegistry(r.ID, hub)
+	return r.ID, nil
+}
+
 // GetRegistry ...
 func GetRegistry(id string) *registry.Registry {
 	return registryMap[id]
@@ -27,6 +52,6 @@ func AddRegistry(id string, r *registry.Registry) {
 }
 
 // RegistryExist ...
-func RegistryExist(r *Registry) bool {
-	return true
+func RegistryExist(r *Registry) (bool, error) {
+	return false, nil
 }
